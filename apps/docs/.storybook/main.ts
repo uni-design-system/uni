7d0372ebb0a8b8045bec85b4d8f8ecbaa846522c
George English <gaenglish@gmail.com>
2026-05-07 18:34:00 -0400

    Add React-Vite Storybook configuration with library references

---
+++ apps/docs/.storybook/main.ts
@@ -0,0 +1,8 @@
+export default {
+  framework: '@storybook/react-vite',
+  stories: [],
+  refs: {
+    react: { title: 'React Library', url: 'http://localhost:6006' },
+    angular: { title: 'Angular Library', url: 'http://localhost:6007' },
+  },
+};
