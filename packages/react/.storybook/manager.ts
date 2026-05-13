b0a2b0d81cfedd09e31febea9d733c54402ff3aa
George English <gaenglish@gmail.com>
2026-05-13 16:36:44 -0400

    Add custom Storybook theme and logo for React package

---
+++ packages/react/.storybook/manager.ts
@@ -0,0 +1,13 @@
+import { addons } from 'storybook/manager-api';
+import { create } from "storybook/theming";
+
+const theme = create({
+  base: 'light',
+  brandTitle: 'UNI Design System',
+  brandUrl: 'https://uni-design-system.github.io/uni/docs/react',
+  brandImage: 'uni-storybook-logo.png',
+
+  appBg: 'white'
+});
+
+addons.setConfig({ theme });
