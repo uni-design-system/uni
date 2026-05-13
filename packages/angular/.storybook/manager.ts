1ac91df2effbe7cce0470a370838e33e68b5d6df
George English <gaenglish@gmail.com>
2026-05-13 16:02:19 -0400

    Add custom Storybook theme and logo for Angular package

---
+++ packages/angular/.storybook/manager.ts
@@ -0,0 +1,4 @@
+import { addons } from 'storybook/manager-api';
+import { default as theme } from './theme';
+
+addons.setConfig({ theme });
---
+++ packages/angular/.storybook/theme.ts
@@ -0,0 +1,11 @@
+import {create} from "storybook/theming";
+
+
+export default create({
+  base: 'light',
+  brandTitle: 'UNI Design System',
+  brandUrl: 'https://uni-design-system.github.io/uni-react/',
+  brandImage: 'uni-storybook-logo.png',
+
+  appBg: 'white'
+});
