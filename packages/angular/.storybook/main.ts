4ec6ee2608af3b1e0f93686cb2b7a858c01e3c0e
George English <gaenglish@gmail.com>
2026-05-18 13:59:54 -0400

    Refactor Angular Storybook configuration to include MDX stories, update framework and builder setup, and enhance type definitions

--- packages/angular/.storybook/main.ts
+++ packages/angular/.storybook/main.ts
@@ -1,19 +1,26 @@
-import type { StorybookConfig } from '@analogjs/storybook-angular';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { mergeConfig } from 'vite';
+import type { StorybookConfig as StorybookConfigBase } from '@storybook/angular';
+import type { StorybookConfigVite } from '@storybook/builder-vite';
 
 function getAbsolutePath(value: string) {
   return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
 }
 
+type StorybookConfig = StorybookConfigBase & StorybookConfigVite;
+
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [getAbsolutePath('@storybook/addon-a11y'), getAbsolutePath('@storybook/addon-docs')],
   framework: {
-    // Switch this to the Analog framework to fix the 'undefined' error
-    name: getAbsolutePath('@analogjs/storybook-angular'),
-    options: {},
+    name: getAbsolutePath('@storybook/angular') as '@storybook/angular',
+    options: {
+      builder: {
+        name: getAbsolutePath('@storybook/builder-vite') as '@storybook/builder-vite',
+        options: {},
+      },
+    },
   },
   staticDirs: ['../../../public'],
   managerHead: (head) => `${head}<link rel="icon" href="/favicon.ico" />`,
