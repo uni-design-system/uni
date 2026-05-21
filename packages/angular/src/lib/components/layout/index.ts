075cfa9b9cf26664ecc8ea6f3885174e41e8b99e
George English <gaenglish@gmail.com>
2026-05-20 22:50:08 -0400

    feat: export layout components from Angular package

---
+++ packages/angular/src/lib/components/layout/index.ts
@@ -0,0 +1,7 @@
+export { UniBoxComponent } from './box/box.component';
+export { UniCenterComponent } from './center/center.component';
+export { UniGridAreaComponent } from './grid/grid-area/grid-area.component';
+export { UniGridComponent } from './grid/grid.component';
+export { UniRowComponent } from './row/row.component';
+export { UniStackComponent } from './stack/stack.component';
+export { UniWrapComponent } from './wrap/wrap.component';
