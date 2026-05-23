7b048308ef5c283dcc586ab71a069a950978c274
George English <gaenglish@gmail.com>
2026-05-22 23:01:14 -0400

    feat: expose Notifications components (Alert, Snackbar, Confirmation Dialog) in public API

---
+++ packages/angular/src/lib/components/notifications/index.ts
@@ -0,0 +1,4 @@
+export * from './alert/alert.component';
+export * from './confirmation-dialog/confirmation-dialog.component';
+export * from './notifications/notifications.component';
+export * from './snackbar/snackbar.component';
